76b1ad33699c45d742cf9f49aaa7ab60b558b509
Sam Williams <samuel.williams@gmail.com>
2015-12-17 12:16:15 -0500

    Initial Commit

---
+++ MarkovKit/MarkovKit.h
@@ -0,0 +1,19 @@
+//
+//  MarkovKit.h
+//  MarkovKit
+//
+//  Created by Sam Williams on 12/17/15.
+//  Copyright © 2015 Sam Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarkovKit.
+FOUNDATION_EXPORT double MarkovKitVersionNumber;
+
+//! Project version string for MarkovKit.
+FOUNDATION_EXPORT const unsigned char MarkovKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkovKit/PublicHeader.h>
+
+
---
+++ MarkovKitTests/MarkovKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarkovKitTests.swift
+//  MarkovKitTests
+//
+//  Created by Sam Williams on 12/17/15.
+//  Copyright © 2015 Sam Williams. All rights reserved.
+//
+
+import XCTest
+@testable import MarkovKit
+
+class MarkovKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
